4d433636664b6e270ce7bd84d8b69f344602634c
aboutaaron <aaron.colby.williams@gmail.com>
2013-02-18 00:10:56 -0800

    added three new drinks

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,7 +10,10 @@ $(function() {
         { name: 'cappuccino', milk: 3, espresso: 2, syrup: 0, water: 0 },
         { name: 'americano', milk: 0, espresso: 2, syrup: 0, water: 3 },
         { name: 'latte', milk: 5, espresso: 2, syrup: 0, water: 0 },
-        { name: 'mocha', milk: 4, espresso: 2, syrup: 1, water: 0 }
+        { name: 'mocha', milk: 4, espresso: 2, syrup: 1, water: 0 },
+        { name: 'special drink', milk: 3.5, espresso: 0, syrup: 3.5, water: 0 },
+        { name: 'cho bro', milk: 1, espresso: 2, syrup: 1, water: 0 },
+        { name: 'devito', milk: 0, espresso: 2, syrup: 0, water: 1.5 }
     ];
 
     new app.CafeView(coffees)
